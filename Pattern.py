a71a3b11fd9d1324fc97b22b0e1983b03334fb57
chaewon <chaewonyun21@gmail.com>
2020-06-11 21:34:26 +0900

    add

---
+++ Pattern.py
@@ -0,0 +1,9 @@
+class Pattern:
+    def __init__(self, name):
+        self.name = name
+        self.category = 0
+        self.needle = list()
+        self.language = list()
+        self.guage = list()
+        self.yardage = list()
+        self.designer = ''
---
+++ Yarn.py
@@ -0,0 +1,9 @@
+class Yarn:
+    def __init__(self, name):
+        self.name = name
+        self.quantity = 0
+        self.needle = list()
+        self.material = list()
+        self.guage = list()
+        self.yardage = list()
+        self.producer = ''
