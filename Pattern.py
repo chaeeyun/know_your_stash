d338e76dadf468dd89b4cee3c95c3e45e4f54ee1
chaewon <chaewonyun21@gmail.com>
2020-09-14 20:41:20 +0200

    remove scripts

--- Pattern.py
+++
@@ -1,9 +0,0 @@
-class Pattern:
-    def __init__(self, name):
-        self.name = name
-        self.category = set()
-        self.needle = set()
-        self.language = set()
-        self.guage = set()
-        self.yardage = set()
-        self.designer = ''
--- Yarn.py
+++
@@ -1,9 +0,0 @@
-class Yarn:
-    def __init__(self, name):
-        self.name = name
-        self.quantity = 0
-        self.needle = set()
-        self.material = set()
-        self.guage = set()
-        self.yardage = set()
-        self.producer = ''
